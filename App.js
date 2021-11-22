5d4bf07e3b8b76bcb6c76fa7ef5b3ed4f4759006
silvercastel <68528032+silvercastel@users.noreply.github.com>
2021-11-22 17:40:43 +0900

    1119 updates
    These ar the newest

--- App.js
+++ App.js
@@ -1,3 +1,76 @@
-import App from './src/App';
+
+import React, {useState} from 'react';
+import TodoList from './components/TodoList';
+import {SafeAreaView, Text, StyleSheet,ScrollView} from 'react-native';
+import TodoInsert from './components/TodoInsert';
+
+const App = () => {
+  // todos: {id: Number, textValue: string, checked: boolean }
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = text => {
+    setTodos([
+      ...todos,
+      {id: Math.random().toString(), textValue: text, checked: false},
+    ]);
+  };
+
+  const onRemove = id => e => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  const onToggle = id => e => {
+    setTodos(
+      todos.map(todo =>
+        todo.id === id ? {...todo, checked: !todo.checked} : todo,
+      ),
+    );
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.appTitle}>Date</Text>
+      <ScrollView style={styles.card}>
+        <TodoInsert onAddTodo={addTodo} />
+        <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+      </ScrollView>
+      <ScrollView style={styles.card}>
+        <TodoInsert onAddTodo={addTodo} />
+        <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+      </ScrollView>
+      <ScrollView style={styles.card}>
+        <TodoInsert onAddTodo={addTodo} />
+        <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+  },
+  appTitle: {
+    color: '#fff',
+    fontSize: 36,
+    marginTop: 30,
+    marginBottom: 30,
+    marginLeft: 10,
+    fontWeight: '300',
+    textAlign: 'left',
+    backgroundColor: '#3143e8',
+  },
+  card: {
+    backgroundColor: '#fff',
+    flex: 1,
+    borderTopLeftRadius: 10, 
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10,
+    borderBottomLeftRadius: 10,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+});
 
 export default App;
