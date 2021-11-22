8f45bc95878b796bc177b0fc25167a1df3c791f6
silvercastel <68528032+silvercastel@users.noreply.github.com>
2021-11-22 18:00:14 +0900

    Images for main page
    The images are on GoogleDrive

---
+++ mainIconImages.js
@@ -0,0 +1,3 @@
+//https://drive.google.com/drive/folders/1r1N1TTqya42NQszIOv8l0qOiz-SmYR3
+//we, sohyun and eunseong uploaded the Icons that is required on Google Drive.
+//please click the link and check the image files
