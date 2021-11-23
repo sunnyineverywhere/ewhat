d134d1de32d261d2363a2eb65e9a944bd9158364
silvercastel <68528032+silvercastel@users.noreply.github.com>
2021-11-23 21:26:05 +0900

    1123 updates
    - menu 아이콘 추가

--- image.js
+++ image.js
@@ -1,5 +1,6 @@
 import basicState from '../assets/icons8-full-image-96.png'
 import checkState from '../assets/icons8-rectangle-96-green.png'
+import Menu from '../assets/icons8-menu-30.png'
 import Edit from '../assets/icons8-edit-96.png'
 import Delete from '../assets/icons8-trash-96.png'
 import Review from '../assets/writing.png'
@@ -10,6 +11,7 @@ import Search from '../assets/outline_search_black_18.png'
 export const images = {
     uncompleted: basicState,
     completed: checkState,
+    menu : Menu,
     update: Edit,
     delete: Delete,
     deleteAll: Delete,
