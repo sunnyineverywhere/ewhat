f812bb98f48583692832008659abf9eb649b2512
silvercastel <68528032+silvercastel@users.noreply.github.com>
2021-11-23 20:31:01 +0900

    1123 updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { useState, } from 'react';
-import {Text, SafeAreaView,View, Dimensions, Button, ScrollView, TouchableOpacity} from 'react-native';
-import { viewStyles,textStyles,Buttonstyles } from './styles';
+import { Text, SafeAreaView,View, Dimensions, Button, ScrollView, TouchableOpacity} from 'react-native';
+import { viewStyles,textStyles} from './styles';
 import Input from './components/Input';
 import Task from './components/Task';
 import IconButton from './components/IconButton';
-import MenuButton from './components/MenuButton';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import { images } from './image';
+
+
 
 const HomeScreen = ({navigation}) =>{
    const width = Dimensions.get('window').width;
@@ -29,6 +30,9 @@ const HomeScreen = ({navigation}) =>{
    const _deletAllTask = () => {
       setTasks([]);
    };
+   const _deleteSelectedTask = () => {
+      const selectedTask = []
+   };
    const _sortTasks = () => {
        const sortTask = [...tasks];
        sortTask.sort((a,b) => (a.id-b.id)? 1: -1);
@@ -48,45 +52,50 @@ const HomeScreen = ({navigation}) =>{
    };
    const _onBlur = () => {
       setNewTask('');
-   }
+   };
+ 
     return (
     <SafeAreaView style={viewStyles.container}>
        <View style={viewStyles.header}>
          <Text style ={textStyles.title}>Date: {today}</Text>
          <TouchableOpacity onPress={()=> navigation.navigate('Menu') }>
-            <Text style={textStyles.menu}>
+            <Text>
                menu
             </Text>
          </TouchableOpacity>
-
-
-                  <IconButton type={images.add} onPressOut= {()=> navigation.navigate('AddSubject')}/>
-
+         
+         <IconButton type={images.add} onPressOut= {()=> navigation.navigate('AddSubject')}/>
 
        </View>
-         <Input value={newTask} onChangeText={_handleTextChange}
+       <Input value={newTask} onChangeText={_handleTextChange}
          onSubmitEditing={_addTask} onBlur={_onBlur}/>
-         <TouchableOpacity style={Buttonstyles.button} onPress={_deletAllTask}>
+
+      <View style={viewStyles.button}>
+         <TouchableOpacity  onPress={_deletAllTask}>
             <Text>Delete All</Text>
          </TouchableOpacity>
 
-         <TouchableOpacity style={Buttonstyles.button} onPress={_sortTasks}>
+         <TouchableOpacity onPress={_sortTasks}>
             <Text>Sort Items</Text>
          </TouchableOpacity>
-         <ScrollView width = {width-20}>
+
+         </View>
+         <ScrollView width = {width-20} >
+
             {Object.values(tasks).reverse().map(item => (
-               <Task key = {item.id} item={item} deleteTask={_deleteTask}
+               <Task key = {item.id} item={item} deleteTask={_deleteTask} 
                toggleTask={_toggleTask} updateTask={_updateTask}/>
            ))}
          </ScrollView>
+
     </SafeAreaView>
-
-
+   
+    
    )
 }
 
 const MenuScreen = ({navigation}) => {
-
+   
    return(
       <View style={textStyles.menuText}>
             <MenuButton type={images.setting}
@@ -105,14 +114,14 @@ const ReviewPage = () => {
       <View>
       <Text>Review</Text>
       </View>
-  )
+  );
 }
 const SearchPage = () =>{
    return(
       <View>
       <Text>Search</Text>
       </View>
-   )
+   );
 }
 
 const AddSubjectPage = () =>{
@@ -122,7 +131,6 @@ const AddSubjectPage = () =>{
       </View>
    )
 }
-
 const Stack = createStackNavigator();
 let today = new Date().toString().slice(0,10);
 
