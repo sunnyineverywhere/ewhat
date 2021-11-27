07b407c21ea5cc0bd9575c4d978bbf2d581e1fbd
SEON UI LEE <80109963+sunnyineverywhere@users.noreply.github.com>
2021-11-28 00:13:19 +0900

    Update Stack.js

--- src/navigation/Stack.js
+++ src/navigation/Stack.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import HOME from "../components/Home";
-import FirstSetting from "../components/FirstSetting";
+import HOME from "./pages/Home";
+import FirstSetting from "./pages/FirstSetting";
 
 const Stack = createStackNavigator();
 
