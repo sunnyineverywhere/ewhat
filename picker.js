cdc0090cf9e65e99fe1c2a992747a94ce7a61430
silvercastel <68528032+silvercastel@users.noreply.github.com>
2021-12-01 22:53:47 +0900

    1201 updates
    - checkbox 를 npm 통해 설치하려 했지만 왠지 불가.
    - picker로 다중 선택을 할 수 있을지 도전해 보겠다

---
+++ picker.js
@@ -0,0 +1,28 @@
+
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, TextInput} from 'react-native';
+import {Picker} from '@react-native-picker/picker';
+
+class PickerComponent extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Picker style={{height: 50, width: 250}}>
+          <Picker.Item label="korea" value={'할일1'} />
+          <Picker.Item label="USA" value={'할일2'} />
+        </Picker>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 20,
+    marginBottom: 200,
+    alignItems: 'center',
+  },
+});
+
+export default PickerComponent;
