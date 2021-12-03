b5402ca035409ac7ca63f65f46d2c8f738c5b7b1
RyuRyu <79790536+Ryuryu128@users.noreply.github.com>
2021-12-03 11:52:15 +0900

    CalendarScreen
    Calendar Page

---
+++ src/navigation/pages/CalendarScreen.js
@@ -0,0 +1,8 @@
+import React, {useContext, useMemo, useState} from 'react';
+import CalendarView from '../components/CalendarView';
+
+function CalendarScreen() {
+  return <CalendarView/>;
+}
+
+export default CalendarScreen;
