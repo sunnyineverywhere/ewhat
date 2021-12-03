be8a2799bfe2966f90403b5b6616bad88c3b4a22
RyuRyu <79790536+Ryuryu128@users.noreply.github.com>
2021-12-03 09:56:54 +0900

    Create CalendarView.js

---
+++ src/components/CalendarView.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Calendar} from 'react-native-calendars';
+import {StyleSheet} from 'react-native';
+
+function CalendarView() {
+  const markedDate = {
+        '2017-12-03' : {
+            selected: true,
+        },
+    },
+
+  return (
+    <Calendar
+      style={styles.calendar}
+      markedDates={markedDate}
+      theme={{
+        selectedDayBackgroundColor: '#009688',
+        arrowColor: '#009688',
+        dotColor: '#009688',
+        todayTextColor: '#009688',
+      }}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  calendar: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#e0e0e0',
+  },
+});
+
+export default CalendarView;
