43cd2ae5caf3b1a5e5f11fcd05ff457592cee573
SEON UI LEE <80109963+sunnyineverywhere@users.noreply.github.com>
2021-12-05 23:54:51 +0900

    subject context api 수정

---
+++ src/components/context/subject.js
@@ -0,0 +1,22 @@
+// subject numbers context(global value) hold
+// subject.js
+// subject의 개수를 관리합니다.
+
+import React, {createContext, Component, useState} from "react";
+
+const subjectContext = createContext({
+    subject: {number: 6},
+    dispatch: () => {},
+});
+
+const subjectProvider = ({children}) => {
+    const [count, setcount] = useState(0);
+
+    const value = {subject: {count}, dispatch: setcount};
+    return <subjectContext.Provider value = {value}>{children}</subjectContext.Provider>
+};
+
+const subjectConsumer = subjectContext.Consumer;
+
+export {subjectConsumer, subjectProvider};
+export default subjectContext;
