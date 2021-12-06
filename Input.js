5bc1a09755b62fe72cd0ade140f7bf8d7d874347
silvercastel <68528032+silvercastel@users.noreply.github.com>
2021-12-06 10:08:27 +0900

    1206 updates
    - text value를 받아서 task
    생성하는 js

---
+++ Input.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Dimensions, StyleSheet, TextInput } from "react-native";
+import {theme} from '../theme';
+
+const Input = ({value, onChangeText, onSubmitEditing, onBlur }) =>{
+    return (
+        <TextInput style = {inputStyle.textInput}
+            placeholder="What to do"
+            placeholderTextColor={theme.main}
+            maxLength={20}
+            keyboardAppearance="dark"
+            value={value} onChangeText={onChangeText}
+            onSubmitEditing={onSubmitEditing}
+            onBlur={onBlur}
+        > 
+        </TextInput>
+    );
+};
+
+const inputStyle = StyleSheet.create({
+    textInput:{
+        fontSize: 25,
+        width: Dimensions.get('window').width-20,
+        height: 50,
+        marginTop: 10,
+        marginLeft: 4,
+        marginBottom: 4,
+        paddingLeft: 15,
+        paddingTop: 2,
+        borderRadius: 10,
+        backgroundColor: theme.itemBackground,
+        color: theme.text,
+        
+    },
+});
+
+export default Input;
