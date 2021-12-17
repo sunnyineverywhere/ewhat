5e86b3baa349d04b67521b033ac81a66c6e78dfe
RyuRyu <79790536+Ryuryu128@users.noreply.github.com>
2021-12-18 00:31:59 +0900

    SearchScreen 구현 완료

--- src/navigation/pages/SearchScreen.js
+++ src/navigation/pages/SearchScreen.js
@@ -3,23 +3,39 @@ import {StyleSheet, Text, View} from 'react-native';
 import SearchHeader from '../../components/SearchScreen/SearchHeader';
 import EmptySearchResult from '../../components/SearchScreen/EmptySearchResult';
 import SearchContext from '../../components/context/SearchContext';
+import LogContext from '../../components/context/LogContext';
+
 
 function SearchScreen({navigation}) {
-  const keyword = useContext(SearchContext);
+  const {keyword} = useContext(SearchContext);
+  const {logs} = useContext(LogContext);
+
+  const filtered = 
+    keyword === ''
+      ? []
+      : logs.filter((log) =>
+        [log.title, log.body].some((text) => text.includes(keyword)),
+      );
 
   if (keyword === ''){
     return <EmptySearchResult type="EMPTY_KEYWORD" />;
   }
 
+  if (filtered.length === 0) {
+    return <EmptySearchResult type="NOT_FOUND" />;
+  }
+
   return (
     <View style={styles.block}>
-        <Text>{keyword}</Text>
+        <FeedList logs={filtered} />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  block: {},
+  block: {
+    flex: 1,
+  },
 });
 
-export default  SearchScreen;
+export default SearchScreen;
