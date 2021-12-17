3fc1de7f34a317ad05f32ceae23554aadf431cd4
RyuRyu <79790536+Ryuryu128@users.noreply.github.com>
2021-12-18 01:20:39 +0900

    결색 결과 리스트의 각 아이템 구현

--- src/components/SearchScreen/SearchListItem.js
+++ src/components/SearchScreen/SearchListItem.js
@@ -5,6 +5,7 @@ import {ko} from 'date-fns/locale';
 import {useNavigation} from '@react-navigation/native';
 
 function formatDate(date) {
+  
   const d = new Date(date);
   const now = Date.now();
   const diff = (now - d.getTime()) / 1000;
