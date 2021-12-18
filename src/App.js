5246231e1d77e81dda77228d5bda4321def84621
silvercastel <68528032+silvercastel@users.noreply.github.com>
2021-12-18 17:20:51 +0900

    1218

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import * as React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import StackNavigation from "./navigation/Stack";
-import { LogContextProvider } from "./components/context/LogContext";
-import { SearchContextProvider } from "./components/context/SearchContext";
-import { SubjectContextProvider } from "./components/context/SubjectContext";
-
-
-const App = () => {
-    return(
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import HOME from "../src/navigation/pages/Home";
+import Main from "../src/navigation/pages/Main";
+import { NavigationContainer } from '@react-navigation/native';
+const Stack = createStackNavigator();
+const App = ()=> {
+    return (
+        <NavigationContainer independent = {true}>
+        <Stack.Navigator initialRouteName = "Home">
+        <Stack.Screen name = "Home" component = {HOME}/>
+        <Stack.Screen name = "Main" component={Main}/>
         
-        <NavigationContainer independent={true}>
-            <LogContextProvider>
-                <SearchContextProvider>
-                    <SubjectContextProvider>
-                         <StackNavigation />
-                    </SubjectContextProvider>
-                </SearchContextProvider>
-            </LogContextProvider>
+        </Stack.Navigator>
         </NavigationContainer>
-    )
-};
+        
+    );
+}
 
 
 export default App;
