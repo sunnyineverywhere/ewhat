a0b36ed95419ae88ebfc4abd316df2f49881f7cf
silvercastel <68528032+silvercastel@users.noreply.github.com>
2021-12-18 17:22:28 +0900

    1218

---
+++ src/components/Main/IconButton.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Pressable,StyleSheet,View,Image } from "react-native";
+import { theme } from "../theme";
+import PropTypes from 'prop-types';
+
+
+const IconButton = ({type, onPressOut, id}) => {
+    const _onPressOut = () => {
+        onPressOut(id);
+    };
+    return(
+        <Pressable onPressOut={_onPressOut}>
+            <Image source={type} style={iconStyle.icon}/>
+        </Pressable>
+    );
+};
+IconButton.defaultProps = {
+    onPressOut: () => {},
+};
+
+
+
+
+const iconStyle = StyleSheet.create({
+    icon:{
+        tintColor: theme.text,
+        width: 30,
+        height: 30,
+        margin: 10,
+    },
+});
+
+IconButton.PropTypes ={
+    type: PropTypes.oneOf(Object.values(images)).isRequired,
+    onPressOut: PropTypes.func,
+    id: PropTypes.string,
+};
+
+export default IconButton;
