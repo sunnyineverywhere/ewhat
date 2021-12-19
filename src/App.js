8f453b84d3aa88729401b7e736b4ae41e5fea659
SEON UI LEE <sunnyinsummer@ewhain.net>
2021-12-19 10:33:57 +0900

    src/app.js merge

--- src/App.js
+++ src/App.js
@@ -3,17 +3,27 @@ import { createStackNavigator } from "@react-navigation/stack";
 import HOME from "../src/navigation/pages/Home";
 import Main from "../src/navigation/pages/Main";
 import { NavigationContainer } from '@react-navigation/native';
+import { LogContextProvider } from "./components/context/LogContext";
+import { SearchContextProvider } from "./components/context/SearchContext";
+import { SubjectContextProvider } from "./components/context/SubjectContext";
+
 const Stack = createStackNavigator();
 const App = ()=> {
     return (
-        <NavigationContainer independent = {true}>
+        
+        <LogContextProvider>
+        <SearchContextProvider>
+            <SubjectContextProvider>
+            <NavigationContainer independent = {true}>
         <Stack.Navigator initialRouteName = "Home">
         <Stack.Screen name = "Home" component = {HOME}/>
         <Stack.Screen name = "Main" component={Main}/>
         
         </Stack.Navigator>
         </NavigationContainer>
-        
+            </SubjectContextProvider>
+        </SearchContextProvider>
+    </LogContextProvider>
     );
 }
 
